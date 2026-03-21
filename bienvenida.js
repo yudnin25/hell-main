686f5d6dce50d40c593982c6f19635511bab178c
yudnin25 <yudnin25@gmail.com>
2026-03-21 06:14:11 -0500

    s

--- bienvenida.js
+++ bienvenida.js
@@ -39,16 +39,27 @@ export function iniciarBienvenida() {
 
     // 📝 5. INYECTAR PRESENTACIÓN EN EL MAIN
     slots.main.innerHTML = `
+
         <div class="presentacion-box">
+        
+       <div id="panorama" style="width: 100%; height: 100vh;"></div>
+
             <h1>Bienvenido a Terabit</h1>
             <p>Tu plataforma de Chat, Tiendas e Inteligencia Artificial.</p>
             <button class="btn-entrar" id="btn-entrar-sistema">ENTRAR AL SISTEMA</button>
         </div>
     `;
-
     // ✨ 6. TEXTO DEL FOOTER
     slots.footer.innerHTML = `<span>CONECTADO AL SISTEMA TERABIT V1.0</span>`;
-
+// Pégalo después de definir los slots.main.innerHTML
+setTimeout(() => {
+    pannellum.viewer('panorama', {
+        "type": "equirectangular",
+        "panorama": "css/img/tienda1.jpg", // Asegúrate que la ruta sea correcta
+        "autoLoad": true,
+        "autoRotate": -2
+    });
+}, 100); // El pequeño retraso asegura que el HTML ya exista en la pantalla
     // ⚡ 7. LÓGICA DE NAVEGACIÓN (LOS DISPARADORES)
 
     // A. El botón de la esquina (INICIAR)
