9e591761c6ed67bf33c1ccbb15e12c22bf121610
yudnin25 <yudnin25@gmail.com>
2026-03-21 08:00:45 -0500

    r

--- bienvenida.js
+++ bienvenida.js
@@ -57,7 +57,7 @@ setTimeout(() => {
         "type": "equirectangular",
         "panorama": "css/img/images.webp", // Asegúrate que la ruta sea correcta
         "autoLoad": true,
-        "autoRotate": -2
+        "autoRotate": "anonymous" 
     });
 }, 100); // El pequeño retraso asegura que el HTML ya exista en la pantalla
     // ⚡ 7. LÓGICA DE NAVEGACIÓN (LOS DISPARADORES)
